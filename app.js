39402544ef22a0a30921a60466522c9b71612cfa
mukila210 <mukilamurugan0210@gmail.com>
2025-05-05 16:36:53 +0530

    Updated project with contact form

--- app.js
+++ app.js
@@ -27,3 +27,24 @@ var sidemenu = document.getElementById("sideMenu");
    function closemenu(){
       sidemenu.style.right = "-150px";
    }
+
+
+  const scriptURL = "https://script.google.com/macros/s/AKfycbzC06NsHg5Smiu7yTlRrv8yVvo_p7m9XjE6b0HAextwHxr1b0WJa3Mqz-D7_Tzhy6Pb/exec"
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+         msg.innerHTML = "Message sent successfully"
+         setTimeout(function(){
+            msg.innerHTML = ""
+         },3000)
+         form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
+
+
+  
